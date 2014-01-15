c0941dd97442bb82f4b100359a9a55cc8795cc60
Damien Lebrun <dinoboff@hotmail.com>
2014-01-15 15:20:56 +0000

    Add dataset service

--- app/assets/app.js
+++ app/assets/app.js
@@ -44091,9 +44091,24 @@ angular.module('angularSpinkit').run(['$templateCache', function($templateCache)
 
 })();
 ;(function () {
-'use strict';
+  'use strict';
 
-  angular.module('myApp.services', ['myApp.config']);
+  angular.module('myApp.services', ['myApp.config', 'ngResource']).
+
+    factory('dataset', function($resource, API_BASE){
+      var res = $resource(API_BASE + '/:key', {key:'@key'});
+
+      return {
+        all: function() {
+          return res.query().$promise;
+        },
+        get: function(key) {
+          return res.get({key: key}).$promise;
+        }
+      };
+    })
+    
+    ;
 
 })();
 ;
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,10 +3,66 @@
 (function () {
   'use strict';
 
-  describe('directives', function(){
+  describe('services', function(){
+    var $httpBackend, scope;
 
     beforeEach(module('myApp.services'));
 
+    beforeEach(inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      scope = $rootScope.$new();
+    }));
+
+    describe('dataset', function() {
+
+      describe('all', function(){
+
+        it('should return a promise', inject(function(dataset) {
+          $httpBackend.whenGET('/api/v1').respond([]);
+          expect(dataset.all().then).toBeDefined();
+        }));
+
+        it('should send a XHR requerest', inject(function(dataset) {
+          var data;
+          
+          $httpBackend.expectGET('/api/v1').respond([{key:0}]);
+          
+          dataset.all().then(function(resp){
+            data = resp;
+          });
+
+          $httpBackend.flush();
+          expect(data.length).toBe(1);
+          expect(data[0].key).toBe(0);
+        }));
+
+      });
+
+      describe('get', function(){
+
+        it('should return a promise', inject(function(dataset) {
+          $httpBackend.whenGET('/api/v1/0').respond({});
+          expect(dataset.get().then).toBeDefined();
+        }));
+
+        it('should send a XHR requerest', inject(function(dataset) {
+          var data;
+          
+          $httpBackend.expectGET('/api/v1/0').respond(
+            { title: 'Distribution of some Data'}
+          );
+          
+          dataset.get(0).then(function(resp){
+            data = resp;
+          });
+
+          $httpBackend.flush();
+          expect(data.title).toBe('Distribution of some Data');
+        }));
+
+      });
+
+    });
 
   });
 
