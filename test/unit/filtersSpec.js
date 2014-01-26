2ad59af2af63b8ef06aff13074e3c900a6bff2ed
Damien Lebrun <dinoboff@hotmail.com>
2014-01-26 18:56:13 +0000

    add test to round filter

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -3,10 +3,24 @@
 (function () {
   'use strict';
 
-  describe('directives', function(){
+  describe('filter', function(){
+    var filter;
 
     beforeEach(module('myApp.filters'));
 
+    beforeEach(inject(function($filter) {
+      filter = $filter;
+    }));
+
+    describe('round', function(){
+      it('should round number to chosen precision', function() {
+        expect(filter('round')(4/3, 2)).toBe(1.33);
+      });
+
+      it('should 0 precision as default', function() {
+        expect(filter('round')(4/3)).toBe(1);
+      });
+    });
 
   });
 
